735cee9c51b04eb027318b8eb00934cfdd08aab3
Camryn Sumabat <51983267+camrynsumabat@users.noreply.github.com>
2021-03-16 15:39:16 -0700

    criminal intent kotlin files + database

---
+++ criminalintent/database/CrimeDatabase.kt
@@ -0,0 +1,15 @@
+package android.bignerdranch.criminalintent.database
+
+import android.bignerdranch.criminalintent.Crime
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+
+@Database(entities = [ Crime::class ], version = 1)
+
+@TypeConverters(CrimeTypeConverters::class)
+
+abstract class CrimeDatabase : RoomDatabase() {
+    abstract fun crimeDao(): CrimeDao
+}
